f6d4952bb6f97cf65e2c69cf15a37fd6cc9c97f2
Adam Toth <tothadam.onp@gmail.com>
2024-02-27 23:11:05 +0100

    cleanup

--- client/src/app/stream.service.ts
+++ client/src/app/stream.service.ts
@@ -1,19 +1,18 @@
-import { Buffer } from 'buffer';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import io from 'socket.io-client';
 
 @Injectable({
     providedIn: 'root',
 })
-export class StreamService {
+export class StreamService implements OnDestroy {
     public socket;
     public incoming = new BehaviorSubject('assets/image/back1.jpg');
     public outgoing = new BehaviorSubject('assets/image/back2.jpg');
     public weight = new BehaviorSubject(0);
 
     constructor() {
-        this.socket = io(``, { upgrade: true, path: `/stream` });
+        this.socket = io(``, { upgrade: true, path: `/stream`, transports: ['websocket']});
 
         this.socket.on('close', () => {
             console.log('ended');
@@ -36,5 +35,12 @@ export class StreamService {
 
     getWeight() {
         return this.weight;
+    }
+
+    ngOnDestroy(): void {
+        this.socket.disconnect();
+        this.incoming.complete();
+        this.outgoing.complete();
+        this.weight.complete();
     }
 }
