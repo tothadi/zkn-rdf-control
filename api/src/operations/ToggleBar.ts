9e0d4a433afa208432b632afe49d1ed5dac70303
Adam Toth <tothadam.onp@gmail.com>
2024-03-18 23:37:33 +0100

    adjust endpoint for new parameters

--- api/src/operations/ToggleBar.ts
+++ api/src/operations/ToggleBar.ts
@@ -1,21 +1,26 @@
 import { Request, Response } from 'express';
 import { logger } from '../utils';
 import { Services } from '../dependencies/configureServices';
+import { Direction, Toggle } from '../types';
 
 export default function ToggleBar({ services }: { services: Services }) {
     return async function toggleBar(req: Request, res: Response) {
         try {
-            const sw = parseInt(req.query.sw as string, 10);
+            const { bar, toggle } = req.query as { bar: Direction; toggle: Toggle };
 
-            if (Number.isNaN(sw) || ![1, 2, 3, 4].includes(sw)) {
-                return res.status(400).json({ message: 'Switch must be one of 1, 2, 3, 4' });
+            if (!['INCOMING', 'OUTGOING'].includes(bar)) {
+                return res.status(400).json({ message: 'Bar can be "INCOMING" or "OUTGOING".' });
             }
 
-            await services.toggleBar(sw);
+            if (!['open', 'close'].includes(toggle)) {
+                return res.status(400).json({ message: 'Toggle can be "open" or "close".' });
+            }
+
+            await services.toggleBar(bar, toggle);
 
             return res.status(200).json({ message: 'OK' });
         } catch (err) {
-            logger('BAR', `${err}`, true);
+            logger('BAR', err as Error);
             return res.status(500).json({ message: 'Server error.' });
         }
     };
